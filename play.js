78e0e3785d9bb20f6d9a0b941713679d24dbf5c8
jiveace <straightedgescotsman@gmail.com>
2018-10-30 10:21:43 +0000

    Adding introduction to Unit Testing

--- play.js
+++ play.js
@@ -1,5 +1,28 @@
 "use strict"
 
-const message = "Back to basics"
-alert(`String interpolated message read "${message}"`);
-alert(-"+6" + -7)
+let pt = function playtime() {
+  const message = "Back to basics"
+  alert(`String interpolated message read "${message}"`);
+  alert(-"+6" + -7)
+
+  if (confirm("Do you want to run the prompt exercise?")) {
+    let gibberish = prompt("You're turn!  What do you have to say for yourself?")
+    console.log(`You said "${gibberish}".  How wise.`)
+  } else {
+    console.log(`You didn't want to talk today. Is it zen, or is it sad?`)
+  }
+};
+
+function email(name, domain = "scottlogic.co.uk") {
+  console.log(`Send mail to ${name}@${domain}`)
+}
+
+pt()
+email("imaxwell")
+email("straightedgescotsman", "gmail.com")
+
+let sum = (input1, input2) => input1 + input2
+alert(sum(6,9))
+
+let double = n => n * 2
+alert(double(9))
---
+++ unit-testing-demo/test.js
@@ -0,0 +1,20 @@
+describe("pow", function() {
+
+  before(() => console.log("Testing started - before all tests"))
+  after(() => console.log("Testing finished - after all tests"))
+
+  beforeEach(() => console.log("Before a test - enter a test"))
+  afterEach(() => console.log("After a test - exit a test"))
+
+  function makeTest(x) {
+    let expected = x * x * x;
+      it(`${x} raised to power 3 is ${expected}`, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+  }
+
+  for (let x = 1; x <=10; x++) {
+    makeTest(x);
+  }
+
+});
